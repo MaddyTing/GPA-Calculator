b04abd6ef164e5ef03fa01d4951ba49217bd47de
MaddyTing <37033263+MaddyTing@users.noreply.github.com>
2019-03-06 10:35:55 -0800

    Add files via upload

---
+++ ting_8_5_1.py
@@ -0,0 +1,26 @@
+# Madeleine Ting
+
+# Period 2
+
+# 8.5 Question 1
+
+def GPAcalc(w):
+    if (w == "A" or w == "a"):
+        return (4)
+    elif (w == "B" or w == "b"):
+        return (3)
+    elif (w == "C" or w == "c"):
+        return (2)
+    elif (w == "D" or w == "d"):
+        return (1)
+    elif (w == "F"or w == "f"):
+        return (0)
+
+#MAIN
+grade = input("Enter your Letter Grade: ")
+
+if(GPAcalc(grade) == None):
+    print("Your GPA score is: Invalid")
+else:
+    print("Your GPA score is: " + str(GPAcalc(grade)))
+
